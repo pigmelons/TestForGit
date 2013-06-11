1084f784d802c06867a22584cd5b0417101a96f9
pigmelons <pigmelons@163.com>
2013-06-11 08:02:48 +0800

    45678

--- TestForGit/FirstViewController.m
+++ TestForGit/FirstViewController.m
@@ -17,10 +17,10 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
+    if (self)
+    {
         self.title = NSLocalizedString(@"First", @"First");
         self.tabBarItem.image = [UIImage imageNamed:@"first"];
-        
     }
     return self;
 }
